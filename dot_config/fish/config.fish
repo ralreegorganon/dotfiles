c8c1b1a9b8922c3d4f1327e2ae957489b56ae73f
Jason Jones <jasonedwardjones@gmail.com>
2025-04-01 14:21:24 -0800

    mise: switch from asdf to mise

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -7,14 +7,13 @@ fish_add_path ~/.cargo/bin
 abbr --add vim nvim
 abbr --add ls eza
 
-# mac version
-#source /usr/local/opt/asdf/libexec/asdf.fish
-# linux version
-source ~/.asdf/asdf.fish
-
 status --is-interactive
 
 zoxide init fish | source
 
+mise activate fish | source
+
 # The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/jj/code/google-cloud-sdk/path.fish.inc' ]; . '/Users/jj/code/google-cloud-sdk/path.fish.inc'; end
+if [ -f '/Users/jj/code/google-cloud-sdk/path.fish.inc' ]
+    . '/Users/jj/code/google-cloud-sdk/path.fish.inc'
+end
