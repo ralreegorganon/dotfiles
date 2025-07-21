a8fcb986f8dd50e1840433864319a94703fb5530
Jason Jones <jasonedwardjones@gmail.com>
2025-07-21 14:57:10 -0800

    fish: add jq-structure command
    Cribbed from https://news.ycombinator.com/item?id=44638356

---
+++ dot_config/fish/functions/jq-structure.fish
@@ -0,0 +1,3 @@
+function jq-structure --description "Extract JSON structure paths using jq"
+    jq -r '[path(..)|map(if type=="number" then "[]" else tostring end)|join(".")|split(".[]")|join("[]")]|unique|map("."+.)|.[]'
+end
